f3ef8978333710945c41ded5e1f8ac91dc3ce091
KARTHIKA VARSHINI <karthikav@infinite.com>
2025-07-06 19:58:31 +0530

    Assignment6 Solutions Updated

---
+++ CSharp/Assignments/Assignment6/Assignment6/question2.cs
@@ -0,0 +1,46 @@
+﻿//2. Write a program in C# Sharp to create a file and write an array of strings to the file.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment6
+{
+    class question2
+    {
+        static void Main()
+        {
+            string[] data =
+            {
+                "Hello!",
+                "This is assignment 6",
+                "Question 2",
+                "Writing an array of strings to the file example",
+                "End of the file!"
+            };
+
+            string filepath = "assign6_q2_file.txt";
+
+            File.WriteAllLines(filepath, data);
+            Console.WriteLine("Array of strings has been written successfully to the file!! \n");
+            try
+            {
+                string[] fileData = File.ReadAllLines(filepath);
+                Console.WriteLine("Reading from the file {0} :", filepath);
+                foreach (string s in fileData)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error in reading the file", e.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Assignments/Assignment6/Assignment6/question3.cs
@@ -0,0 +1,37 @@
+﻿//3. Write a program in C# Sharp to count the number of lines in a file.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment6
+{
+    class question3
+    {
+        static void Main()
+        {
+            string filepath = "assign6_q3_file.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filepath);
+                int count = lines.Length;
+
+                Console.WriteLine($"Number of lines in the file {filepath} are: {count}");
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("Error: File not present at the given path");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error in reading the file", e.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
